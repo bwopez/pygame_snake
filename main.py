353607966b94bc3818de11d9ab1236a42bce02a7
Matthew <matthewbropez@gmail.com>
2021-06-25 11:38:26 -0400

    Uploading the main.py

---
+++ main.py
@@ -0,0 +1,143 @@
+import pygame, random
+from pygame.locals import *
+# window creation =====================================
+pygame.init()
+clock = pygame.time.Clock()
+
+win = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("snek gaem")
+
+
+class Food(pygame.sprite.Sprite):
+    def __init__(self, x=0, y=0):
+        super().__init__()
+        img = pygame.image.load("images/heart.png")
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, imported_img_url, x=0, y=0):
+        super().__init__()
+        img = pygame.image.load(imported_img_url)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y 
+
+        self.step = self.rect.width
+    
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def move(self, direction, your_leader=0):
+
+        if your_leader:
+            self.rect.x, self.rect.y = your_leader.rect.x, your_leader.rect.y
+        else:
+            if direction == "left":
+                self.rect.x += -self.step
+                if self.rect.left <= 0:
+                    self.rect.left = 0
+            elif direction == "right":
+                self.rect.x += self.step
+                if self.rect.right >= win.get_width():
+                    self.rect.right = win.get_width()
+            elif direction == "up":
+                self.rect.y += -self.step
+                if self.rect.top <= 0:
+                    self.rect.top = 0
+            elif direction == "down":
+                self.rect.y += self.step
+                if self.rect.bottom >= win.get_height():
+                    self.rect.bottom = win.get_height()
+
+def main_game():
+    head = Player("images/snake_32b.png")
+    # snake_body1 = Player("images/snake_32.png")
+    # snake_body2 = Player("images/snake_32b.png")
+    # snake_body3 = Player("images/snake_32.png")
+    # snake_list = [head, snake_body1, snake_body2, snake_body3]
+    snake_list = []
+    snake_list.append(head)
+
+    food = Food(random.randint(0, win.get_width()), random.randint(0, win.get_height()))
+
+    running = True
+
+    while running:
+
+        # TODO: kill the heart
+        # TODO: respawn a new heart
+        if head.rect.colliderect(food.rect):
+            print("We've got a hit")
+
+        # Spawn heart in a random spot on the board
+        # if not food:
+        #     # if the snake is in that spot then find another one
+        #     heart_x = random.randint(0, win.width)
+        #     heart_y = random.randint(0, win.height)
+        #     food = Food(heart_x, heart_y)
+
+        # Handle player quit ===============================
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+
+        keys = pygame.key.get_pressed()
+        if keys[K_ESCAPE]:
+            running = False
+        
+        # TODO: randomize where the heart will appear
+        # TODO: when the snake collides with the heart, the heart is destroyed, it addes to the snake_list, new heart appears
+        # TODO: when the snake collides with itself or the wall, it dies and you lose
+        if keys[K_SPACE]:
+            if len(snake_list) % 2 == 0:
+                snake_list.append(Player("images/snake_32b.png", snake_list[-1].rect.x, snake_list[-1].rect.y))
+            else:
+                snake_list.append(Player("images/snake_32.png", snake_list[-1].rect.x, snake_list[-1].rect.y))
+        
+        # Snake movement ====================================
+        if keys[K_a]:
+            for segment in reversed(snake_list):
+                if snake_list.index(segment) == 0:
+                    segment.move("left")
+                else:
+                    segment.move("left", snake_list[snake_list.index(segment) - 1])
+        if keys[K_d]:
+            for segment in reversed(snake_list):
+                if snake_list.index(segment) == 0:
+                    segment.move("right")
+                else:
+                    segment.move("right", snake_list[snake_list.index(segment) - 1])
+        if keys[K_w]:
+            for segment in reversed(snake_list):
+                if snake_list.index(segment) == 0:
+                    segment.move("up")
+                else:
+                    segment.move("up", snake_list[snake_list.index(segment) - 1])
+        if keys[K_s]:
+            for segment in reversed(snake_list):
+                if snake_list.index(segment) == 0:
+                    segment.move("down")
+                else:
+                    segment.move("down", snake_list[snake_list.index(segment) - 1])
+
+        # Update window ===================================
+        win.fill("White")
+        for segment in reversed(snake_list):
+            segment.draw(win)
+        food.draw(win)
+
+        clock.tick(27)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main_game()
+    pygame.quit()
